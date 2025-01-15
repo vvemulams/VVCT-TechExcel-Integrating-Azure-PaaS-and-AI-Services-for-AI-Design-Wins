738939038e6c88000d2e56a2a0194466f6242f1d
Veera Vemula <Veera.Vemula@microsoft.com>
2025-01-15 13:47:17 -0600

    Api endpoint and sql server update

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
 CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+ALTER ROLE db_datareader ADD MEMBER id-q73nmw3temnwc;
+ALTER ROLE db_datawriter ADD MEMBER id-q73nmw3temnwc;
 
